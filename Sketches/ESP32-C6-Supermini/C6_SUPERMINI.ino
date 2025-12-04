72f3ba72e1f0e606257ef052f0aee71e216e987a
Gareth <143119785+gargum@users.noreply.github.com>
2025-12-03 20:55:16 -0500

    Created the ESP32-C6 Supermini's drop-in firmware file

---
+++ Sketches/ESP32-C6-Supermini/C6_SUPERMINI.ino
@@ -0,0 +1,56 @@
+/**
+* @file Tentacle.ino
+* @brief Main sketch for the Tentacle 4x12 keyboard
+*/
+
+#include <SQUIDHID.h>
+
+SQUIDHID esp("SquidHID", "SquidHID", 100);
+
+MATRIX(matrix) = { 
+    {15, 19},{15, 0}, {18, 20},{19, 15},{19, 0}, {20, 19},{16, 15},{16, 0},{17, 20},{0, 15},{0, 16},{3,20}, 
+    {15, 20},{15, 3}, {18, 16},{19, 18},{19, 3}, {20, 17},{16, 18},{16, 3},{17, 16},{0, 18},{0, 17},{3,16}, 
+    {15, 16},{18, 15},{18, 17},{19, 16},{20, 15},{20, 0}, {16, 19},{17,15},{17, 0}, {0, 19},{3, 18},{3,17}, 
+    {15, 17},{18, 19},{18, 3}, {19, 17},{20, 18},{20, 3}, {16, 20},{17,18},{17, 3}, {0, 20},{3, 19},{3,0} 
+};
+
+LAYER(base_layer) = {    
+      KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS, 
+      KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, 
+      KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RCTL, 
+      MO(1),   KC_LGUI, KC_MINS, KC_PGDN, KC_SPC, KC_BSPC, KC_ENT, KC_LSFT, KC_PGUP, KC_EQL, KC_RGUI, MO(2)
+};
+
+LAYER(function_layer) = {
+      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10, KC_F11,  KC_F12, 
+      KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,    KC_6,   KC_7,    KC_8,    KC_9,   KC_0,    KC_QUOT, 
+      KC_LCTL, KC_VOLD, KC_MPRV, KC_MRWD, KC_MSTP,KC_WBAK, KC_WFWD,KC_MPLY, KC_MFFD, KC_MNXT,KC_VOLU, KC_RCTL, 
+      TRANS,   KC_PGDN, KC_DOWN, KC_LEFT, KC_SPC, KC_BSPC, KC_ENT, KC_LSFT, KC_RGHT, KC_UP,  KC_PGUP, TRANS
+};
+
+KEYMAP(layers) = {
+   base_layer,
+   function_layer, 
+};
+
+void setup() {
+ esp.setLogLevel(LOGGER_VERBOSE);
+ esp.setAppearance(KEYBOARD);
+ 
+ esp.begin(matrix, layers);  
+ 
+ esp.initializeLEDs(LED_COUNT, LED_PIN, NEO_GRB);
+ esp.setLEDBrightness(50);
+ esp.fillLEDs(RGB_AZURE);
+
+ esp.oledClear();
+ esp.oledDrawBitmap_P(0, 0, 64, 64, real_squid, OLED::WHITE);
+ esp.oledDrawString(64, 24, "SquidHID", OLED::NORMAL_SIZE, OLED::WHITE);
+ esp.oledDisplayUpdate();
+}
+
+void loop() {  
+
+ esp.update();
+
+}
